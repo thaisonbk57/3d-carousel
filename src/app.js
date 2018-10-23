e88e6a68e629e1578e39c8c5e06d66cf2f4567ed
Thaison Dang <thaisondang.bk57@gmail.com>
2018-10-23 02:34:32 +0200

    finished

---
+++ src/app.js
@@ -0,0 +1,34 @@
+//   transform: translate(-50%, -50%) rotateY(40deg);
+// const $ = require('jquery');
+
+$(document).ready(function() {
+  const carousel = $('#carousel');
+  const itemsCount = 9;
+  let step = 360 / itemsCount;
+  let photoIndex = 0;
+
+  function spinCarousel() {
+    let angle = photoIndex * step;
+    carousel.css('transform', `translate(-50%, -50%) rotateY(-${angle}deg)`);
+  }
+
+  function next() {
+    photoIndex++;
+    spinCarousel();
+  }
+
+  function prev() {
+    photoIndex--;
+    spinCarousel();
+  }
+  $('#next')
+    .unbind('click')
+    .on('click', function() {
+      next();
+    });
+  $('#prev')
+    .unbind('click')
+    .on('click', function() {
+      prev();
+    });
+});
